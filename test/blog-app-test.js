0ad641120c3775a18b7d899b93288d8d0e1f73fb
Marck Manga <Marck@thinkful.com>
2018-07-05 16:36:34 -0700

    Final and Working

--- test/blog-app-test.js
+++ test/blog-app-test.js
@@ -3,7 +3,7 @@ const chaiHttp = require('chai-http');
 const faker = require('faker');
 const mongoose = require('mongoose');
 
-const should = chai.should; 
+const should = chai.should(); 
 
 const {BlogPost} = require('../models');
 const {app, runServer, closeServer} = require('../server');
@@ -11,35 +11,10 @@ const {TEST_DATABASE_URL} =	require('../config');
 
 chai.use(chaiHttp);
 
-/*function seedBlogData() {
-	const seedData = []; 
-
-	for (let i=1; i<=5; i++) {
-		seedData.push(generateBlogData());
-	}
-	return BlogPost.insertMany(seedData);
-}
-
-function generateBlogData() {
-	return {
-		title: generateTitle(),
-		author: {
-			firstName: faker.name.firstName(),
-			lastName: faker.name.lastName(),
-		},
-		content: faker.lorem.text(),
-	};
-}
-
-function generateTitle() {
-	const title = ['Me and Only Me', '1983', 'A Catcher in the Sourdough', 'Narcos', "Moby Shark" ];
-	return title[Math.floor(Math.random() * title.length)];
-}*/
-
 function seedBlogPostData() {
   console.info('seed blog data');
   const seedData = [];
-  for (let i = 1; i <= 10; i++) {
+  for (let i = 1; i <= 5; i++) {
     seedData.push({
       author: {
         firstName: faker.name.firstName(),
@@ -79,18 +54,18 @@ describe('Blogs API Resource', function() {
 
 	describe('GET Endpoint', function() {
 		it('should return all blogs', function() {
-			let res;
 			return chai.request(app)
 				.get('/posts')
-				.then(_res => {
-					res = _res;
+				.then(res => {
 					res.should.have.status(200);
 					res.body.should.have.lengthOf.at.least(1);
 					return BlogPost.count();
-				})
-				.then(count => {
 					res.body.should.have.lengthOf(count);
-				});
+					//return BlogPost.count();
+				})
+				/*.then(count => {
+					res.body.should.have.lengthOf(count);
+				});*/
 		});
 		it('should return posts with correct fields', function() {
 			let respost;
@@ -99,7 +74,7 @@ describe('Blogs API Resource', function() {
 			.then(function(res) {
 				res.should.have.status(200);
 				res.should.be.json;
-				res.should.be.a('array');
+				res.should.be.a('object');
 				res.body.should.have.lengthOf.at.least(1)
 
 				res.body.forEach(function(post) {
@@ -112,8 +87,106 @@ describe('Blogs API Resource', function() {
 			.then(post => {
 				respost.title.should.equal(post.title);
 				respost.content.should.equal(post.content);
-				respost.author.should.equal(post.author);
+				respost.author.should.equal(post.authorName);
 			});
 		});	
 	});
+
+	describe('POST Endpoint', function() {
+		it('should add a new post', function() {
+			const newPost = {
+				title: faker.lorem.sentence(),
+				author: {
+					firstName: faker.name.firstName(),
+       				lastName: faker.name.lastName()
+				},
+				content: faker.lorem.text()
+			};
+			return chai.request(app)
+				.post('/posts')
+				.send(newPost)
+				.then(res => {
+					res.should.have.status(201);
+					res.should.be.json;
+					res.should.be.a('object');
+					res.body.should.include.keys('id', 'title', 'content', 'author', 'created');
+					res.body.title.should.equal(newPost.title);
+          			res.body.id.should.not.be.null;
+          			res.body.author.should.equal(`${newPost.author.firstName} ${newPost.author.lastName}`);
+          			res.body.content.should.equal(newPost.content);
+          			return BlogPost.findById(res.body.id);
+				})
+				.then(function (post) {
+          		post.title.should.equal(newPost.title);
+          		post.content.should.equal(newPost.content);
+          		post.author.firstName.should.equal(newPost.author.firstName);
+          		post.author.lastName.should.equal(newPost.author.lastName);
+          	});
+		});
+	});
+	describe('PUT endpoint', function () {
+
+    // strategy:
+    //  1. Get an existing post from db
+    //  2. Make a PUT request to update that post
+    //  4. Prove post in db is correctly updated
+    	it('should update fields you send over', function () {
+      		const updateData = {
+        		title: 'cats cats cats',
+        		content: 'dogs dogs dogs',
+        		author: {
+          			firstName: 'foo',
+          			lastName: 'bar'
+        		}
+      		};
+
+      		return BlogPost
+        		.findOne()
+        		.then(post => {
+        			updateData.id = post.id;
+
+          			return chai.request(app)
+            		.put(`/posts/${post.id}`)
+            		.send(updateData);
+        		})
+        		.then(res => {
+          			res.should.have.status(204);
+          			return BlogPost.findById(updateData.id);
+        		})
+        		.then(post => {
+          			post.title.should.equal(updateData.title);
+          			post.content.should.equal(updateData.content);
+          			post.author.firstName.should.equal(updateData.author.firstName);
+          			post.author.lastName.should.equal(updateData.author.lastName);
+        		});
+    	});
+  	});
+
+  describe('DELETE endpoint', function () {
+    // strategy:
+    //  1. get a post
+    //  2. make a DELETE request for that post's id
+    //  3. assert that response has right status code
+    //  4. prove that post with the id doesn't exist in db anymore
+    	it('should delete a post by id', function () {
+			let post;
+			return BlogPost
+        		.findOne()
+        		.then(_post => {
+          			post = _post;
+          			return chai.request(app).delete(`/posts/${post.id}`);
+        		})
+        		.then(res => {
+          			res.should.have.status(204);
+          			return BlogPost.findById(post.id);
+        		})
+        		.then(_post => {
+          // when a variable's value is null, chaining `should`
+          // doesn't work. so `_post.should.be.null` would raise
+          // an error. `should.be.null(_post)` is how we can
+          // make assertions about a null value.
+          			should.not.exist(_post);
+        		});
+    	});
+  	});
 });
